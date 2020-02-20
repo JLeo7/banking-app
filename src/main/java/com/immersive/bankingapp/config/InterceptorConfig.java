6edeecb1ccfab98c0d300498acaa424bc8af32ad
Leonardo Mora <jleomoram@gmail.com>
2020-02-20 00:14:14 -0600

    added register and sign-in endpoints to another resource. fixed the config annotation in the interceptor

--- src/main/java/com/immersive/bankingapp/config/InterceptorConfig.java
+++ src/main/java/com/immersive/bankingapp/config/InterceptorConfig.java
@@ -1,12 +1,15 @@
 package com.immersive.bankingapp.config;
 
 import com.immersive.bankingapp.interceptor.JWTInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new JWTInterceptor()).excludePathPatterns("/");
+//        registry.addInterceptor(new JWTInterceptor()).excludePathPatterns("/register");
+        registry.addInterceptor(new JWTInterceptor());
     }
 }
--- src/main/java/com/immersive/bankingapp/resource/AccessResource.java
+++ src/main/java/com/immersive/bankingapp/resource/AccessResource.java
@@ -10,34 +10,22 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("api/user")
-public class UserResource {
+@RequestMapping("api/")
+public class AccessResource {
     private UserService userService;
 
-    public UserResource(UserService userService) {
+    public AccessResource(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<User> save(@RequestBody User user ) {
+    @PostMapping("register")
+    public ResponseEntity<User> register(@RequestBody User user ) {
         User savedUser = this.userService.save(user);
         return new ResponseEntity<>(savedUser, HttpStatus.OK);
     }
 
-    @GetMapping
-    public ResponseEntity<List> findAll() {
-        List<User> users = this.userService.findAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> findById(@PathVariable int userId) {
-        User user = this.userService.findUserById(userId);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
     @GetMapping("/sign-in")
-    public ResponseEntity<User> findById(@RequestBody User user) {
+    public ResponseEntity<User> signIn(@RequestBody User user) {
         user = this.userService.findUserByEmailAndPassword(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
